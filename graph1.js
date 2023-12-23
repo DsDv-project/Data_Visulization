91b3c81d8dc8ddeac0ac20ed9c0b003c111fda9a
driss <dothanhdat185@gmail.com>
2023-12-23 20:32:30 +0700

    Add graph and change position

--- graph1.js
+++ graph1.js
@@ -1,90 +1,67 @@
-d3.csv("netflix_titles_cleaned.csv").then(function(data) {
-    const counts = {
-        "Movie": 0,
-        "TV Show": 0,
-    };
-    data.forEach(function(d) {
-        if (d.type === "Movie") {
-            counts["Movie"] += 1;
-        } else {
-            counts["TV Show"] += 1;
-        }
-    });
+var data = [
+    {year: 2011, users: 21.5},
+    {year: 2012, users: 25.7},
+    {year: 2013, users: 35.6},
+    {year: 2014, users: 47.9},
+    {year: 2015, users: 62.7},
+    {year: 2016, users: 79.9},
+    {year: 2017, users: 99},
+    {year: 2018, users: 124.3},
+    {year: 2019, users: 151.5},
+    {year: 2020, users: 192.9}
+];var margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = 900 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
 
-    const barData = [
-        { "type": "Movie", "count": counts["Movie"] },
-        { "type": "TV Show", "count": counts["TV Show"] }
-    ];
+var svg = d3.select("#graph1")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
-    const margin = {top: 50, right: 10, bottom: 50, left: 50},
-    width = 1000 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+var x = d3.scaleLinear()
+  .domain(d3.extent(data, function(d) { return d.year; }))
+  .range([ 0, width ]);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
-    // Append the svg object to the body of the page
-    var svg = d3.select("#graph1")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-        "translate(" + margin.left + "," + margin.top  + ")");
-    
-    // X axis
-    var x = d3.scaleBand()
-    .range([0, width])
-    .domain(["Movie", "TV Show"]) // Update X-axis domain
-    .padding(0.2);
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .selectAll("text")
-            .attr("transform", "translate(15,5)rotate(0)")
-            .style("font-size", "25px")
-            .style("text-anchor", "end");
-  
-    // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(barData, d => d.count)*1.2]) // Set the upper limit to the max count
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisLeft(y))
-        .selectAll("text")
-            .style("font-size", "17px");
-  
-    // Bars
-    svg.selectAll("mybar")
-    .data(barData) // Use barData here
-    .enter()
-    .append("rect")
-        .attr("x", function(d) { return x(d.type); })
-        .attr("y", function(d) { return y(d.count); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d.count); })
-        .attr("fill", function(d) { 
-            if (d.type === "Movie") {
-                return "red"; 
-            } else {
-                return "black"; 
-            }
-        });
+var y = d3.scaleLinear()
+  .domain([0, d3.max(data, function(d) { return +d.users; })])
+  .range([ height, 0 ]);
+svg.append("g")
+  .call(d3.axisLeft(y));
 
-    svg.append("text")
-        .attr("x", (width / 2))             
-        .attr("y", -25)
-        .attr("text-anchor", "middle")  
-        .style("font-size", "18px") 
-        .style("text-decoration", "bold")  
-        .attr("fill", "white")
-        .text("Number of Movies and TV Shows on Netflix");
+svg.append("path")
+  .datum(data)
+  .attr("fill", "none")
+  .attr("stroke", "red")
+  .attr("stroke-width", 1.5)
+  .attr("d", d3.line()
+    .x(function(d) { return x(d.year) })
+    .y(function(d) { return y(d.users) })
+  )
 
-    svg.selectAll("g g.tick text")
-        .attr("fill", "Ivory");
-    svg.selectAll("g g.tick line")
-        .attr("stroke", "red");
-    svg.selectAll("g path.domain")
-        .attr("stroke", "green")
-        .attr("stroke-width", 3);
+svg.append("g")
+    .attr("transform", "translate(0," + height +")")
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+    .selectAll("text, line") 
+    .attr("stroke", "white")
+    .style("font-size", "16px")
+    .attr("fill", "white"); 
 
-}).catch(function(error) {
-    console.log(error);
-});
+svg.selectAll(".domain")
+    .attr("stroke", "white");
+
+
+svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll("text, line") 
+    .attr("stroke", "white")
+    .style("font-size", "16px") 
+    .attr("fill", "white");
+
+svg.selectAll(".domain")
+    .attr("stroke", "white");
